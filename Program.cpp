225aecb8ba5d783c9cac018d986f9a03b0ab3115
Minh Quân <lemquan6688@gmail.com>
2025-04-17 13:31:48 +0700

    Add Program.cpp to root directory

---
+++ Program.cpp
@@ -0,0 +1,15 @@
+﻿// Program.cpp
+#include "MainForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+int main(cli::array<String^>^ args)
+{
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    AESEncryptionApp::MainForm form;
+    Application::Run(% form);
+    return 0;
+}
